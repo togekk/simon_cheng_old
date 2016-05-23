b23ec43e99434f191da46b59822b206be6e2f174
togekk <togekk@gmail.com>
2016-05-24 06:51:29 +0800

    Change menu image and minor UI update

--- js/script.js
+++ js/script.js
@@ -38,6 +38,7 @@ page.config(function($routeProvider) {
 		rel : 0,
 		showinfo : 0,
 		modestbranding : 1,
+		wmode : "opaque",
 		start : 25,
 		end : 62
 	};
@@ -49,42 +50,6 @@ page.config(function($routeProvider) {
 $scope.$on('youtube.player.ended', function ($event, player) {
 		player.seekTo('25');
 	});
-	
-	// Find all YouTube videos
-var $allVideos = $("iframe[src^='//www.youtube.com']"),
-
-    // The element that is fluid width
-    $fluidEl = $("body");
-
-// Figure out and save aspect ratio for each video
-$allVideos.each(function() {
-
-  $(this)
-    .data('aspectRatio', this.height / this.width)
-
-    // and remove the hard coded width/height
-    .removeAttr('height')
-    .removeAttr('width');
-
-});
-
-// When the window is resized
-$(window).resize(function() {
-
-  var newWidth = $fluidEl.width();
-
-  // Resize all videos according to their own aspect ratio
-  $allVideos.each(function() {
-
-    var $el = $(this);
-    $el
-      .width(newWidth)
-      .height(newWidth * $el.data('aspectRatio'));
-
-  });
-
-// Kick off one resize to fix all videos on page load
-}).resize();
 
 	$scope.close = function() {
 		// vid.pause();
